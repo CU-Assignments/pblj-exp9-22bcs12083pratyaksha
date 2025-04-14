fbb89fd974ff0201a2e8c21150cf12af694e681e
22bcs12083pratyaksha <pratyaksha12083@gmail.com>
2025-04-14 22:11:51 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private int studentId;
+    private String name;
+    private String department;
+    private double marks;
+
+    public Student(int studentId, String name, String department, double marks) {
+        this.studentId = studentId;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    // Getters & Setters
+    public int getStudentId() { return studentId; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getMarks() { return marks; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDepartment(String department) { this.department = department; }
+    public void setMarks(double marks) { this.marks = marks; }
+}
